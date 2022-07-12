9d31acafe0018a060955d164a15214cdbd7dbfc2
Ivanyvienen01 <93613677+Ivanyvienen01@users.noreply.github.com>
2022-07-12 10:29:29 -0400

    Add files via upload

---
+++ Hoja de Vida/index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <head>
+        <title>Curriculum</title>
+        <link rel='stylesheet' href='css/style.css' />
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+            <script src='https://www.google.com/recaptcha/api.js'></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script> 
+        
+</head>
+<body>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+        <a class="navbar-brand" target="_blank" href="">Ivan Rodriguez - Programacion III</a>
+      </nav>
+  
+    <div class="header">
+      <img class="imagen" src="img/imagen.jpg" width="180" height="200">
+      
+    </div>
+  
+    <div class="titulo-info">
+      <h1 class="titulo">Curriculum</h1>
+    </div>
+  
+    <div class="info">
+        <p class="titulo-info">Informacion Personal</p>
+        <p class="subtitulo-info">Nombre y Apellido:</p>
+        <p class="content-info">Iván Jose, Rodríguez Isturíz</p><br><br>
+        <p class="subtitulo-info">Cedula de Identidad:</p>
+        <p class="content-info">30.294.686</p><br><br>
+        <p class="subtitulo-info">Fecha de Nacimiento:</p>
+        <p class="content-info">02/01/2002</p><br><br>
+        <p class="subtitulo-info">Direccion:</p>
+        <p class="content-info">Edo. Aragua - Villa de Cura - Av. Carrizalito #27</p><br><br>
+        <p class="subtitulo-info">Telefono:</p>
+        <p class="content-info">(+58) 412-059-0188</p><br><br>
+        <p class="subtitulo-info">Correo:</p>
+        <p class="content-info">ivanrodri30294686@gmail.com</p><br><br>
+    </div>
+  
+    <div class="info">
+        <p class="titulo-info">Formacion Academica</p>
+        <p class="subtitulo-info">Titulo de Educacion Primaria:</p>
+        <p class="content-info">E.B.N “Melicia Nieves de Tejada”</p><br><br>
+        <p class="subtitulo-info">Titulo de Bachiller en Ciencias:</p>
+        <p class="content-info">U.E.M “Agustín Armario”</p><br><br>
+        <p class="subtitulo-info">Ingeniería en Sistemas:</p>
+        <p class="content-info">U.N.E.R.G” Universidad Nacional Experimental Rómulo Gallegos” (Cursando)</p><br><br>
+    </div>
+  
+    <div class="info">
+        <p class="titulo-info">Experiencia Laboral</p>
+        <p class="subtitulo-info">100% Puro Cuero C.A:</p>
+        <p class="content-info">Supervisor de Linea (10 años)</p><br><br>
+        <p class="subtitulo-info">Direccion:</p>
+        <p class="content-info">Edo. Aragua - Villa de Cura - Av. Carrizalito #27</p><br><br>
+        <p class="titulo-info"></p>
+        <p class="subtitulo-info">Orlando Bomb’s:</p>
+        <p class="content-info">Asistente de Panadero (2 años)</p><br><br>
+        <p class="subtitulo-info">Direccion:</p>
+        <p class="content-info">Edo. Aragua - Villa de Cura - Av. Carrizalito #27</p><br><br>
+        <p class="titulo-info"></p>
+        <p class="subtitulo-info">Gearca C.A:</p>
+        <p class="content-info">Vigilante de Seguridad (4 meses)</p><br><br>
+        <p class="subtitulo-info">Direccion:</p>
+        <p class="content-info">Edo. Miranda - Los Teques - Zona Industrial Guaracarumbo</p><br><br>
+    </div>
+  
+    <div class="info">
+        <p class="titulo-info">Referencia Laboral</p>
+        <p class="subtitulo-info">Gerente:</p>
+        <p class="content-info">Orlando Rodríguez</p><br><br>
+        <p class="subtitulo-info">Empresas:</p><br><br>
+        <p class="content-info">100% Puro Cuero C.A</p><br><br>
+        <p class="content-info">Orlando’s Bombs</p><br><br>
+        <p class="subtitulo-info">Telefono:</p>
+        <p class="content-info">(+58) 412-434-7568</p><br><br>
+        <p class="titulo-info"></p>
+        <p class="subtitulo-info">Gerente:</p>
+        <p class="content-info">Luis Martinez</p><br><br>
+        <p class="subtitulo-info">Empresa:</p>
+        <p class="content-info">Gearca C.A</p><br><br>
+        <p class="subtitulo-info">Telefono:</p>
+        <p class="content-info">(+58) 412-981-9258</p><br><br>
+    </div>
+
+</body>
+</html>
