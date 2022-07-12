d4191b1b606e739c9951d42517c9ec3b72541d03
Ivanyvienen01 <93613677+Ivanyvienen01@users.noreply.github.com>
2022-07-12 11:17:22 -0400

    Add files via upload

---
+++ Ivan Rodriguez/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <head>
+        <title>Estudiante</title>
+        <link rel='stylesheet' href='css/style.css' />
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        
+    </head>
+<body>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+        <a class="navbar-brand" target="_blank" href="">Ivan Rodriguez - Programacion III</a>
+      </nav><br><br><br>
+  
+    <div class="titulo-info">
+      <h1 class="titulo">Estudiante</h1>
+    </div>
+  
+    <div class="info">
+        <p class="subtitulo-info">Nombre y Apellido:</p>
+        <p class="content-info">Iván Jose, Rodríguez Isturíz</p><br><br>
+        <p class="subtitulo-info">Cedula de Identidad:</p>
+        <p class="content-info">30.294.686</p><br><br>
+    </div>
+  
+
+</body>
+</html>
